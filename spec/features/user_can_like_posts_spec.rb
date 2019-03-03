f247436a00efd63387b0026757372914432c6c88
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-03 19:31:31 +0000

    Testing for like and unlike

---
+++ spec/features/user_can_like_posts_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'sign_up_helper'
+
+RSpec.feature "Journey", type: :feature do
+  scenario "can sign up" do
+    sign_up_user
+    click_link("New Post", match: :first)
+    fill_in "post_title", with: "Feature title!"
+    fill_in "post_body", with: "Feature body text!"
+    page.attach_file("post_image", Rails.root + 'app/assets/images/imagename.jpg')
+    click_on "Submit"
+    expect(page.html).to include("imagename.jpg")
+    expect(page).to have_content("Feature body text!")
+    click_button('Like')
+    expect(page).to have_content("1 Like")
+    click_button('Unlike')
+    expect(page).to have_content("0 Likes")
+  end
+end
