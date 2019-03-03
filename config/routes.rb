f50f13425d46a99d72e8c7fe041de100d7c76552
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-03 19:24:50 +0000

    Added Likes model/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+
+  resources :posts do
+    resources :likes
+  end
+  
   devise_for :users, controllers: {
        sessions: 'users/sessions'
      }
